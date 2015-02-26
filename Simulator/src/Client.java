6129a3d01e304a283aa99717b71e94a1b33141ca
qixao001 <miaxiii@gmail.com>
2015-02-26 10:20:01 +0800

    transfer object server

--- Simulator/src/Client.java
+++ Simulator/src/Client.java
@@ -1,25 +1,59 @@
+import java.io.IOException;
 import java.io.*;
-import java.net.*;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client {
-    public static void main(String argv[]) throws Exception {
-        String sentence;
-        String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-
-        Socket clientSocket = new Socket("localhost", 5678);
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        
-        sentence = inFromUser.readLine();
-        while (sentence.equals("exit")){
-     //   sentence = inFromUser.readLine();
-        outToServer.writeBytes(sentence + '\n');
-        modifiedSentence = inFromServer.readLine();
-        System.out.println(modifiedSentence); 
-        sentence = inFromUser.readLine();
+    private Socket socket = null;
+    private ObjectInputStream inputStream = null;
+    private ObjectOutputStream outputStream = null;
+    private boolean isConnected = false;
+ 
+    public Client() {
+ 
+    }
+ 
+    public void communicate() {
+ 
+        while (!isConnected) {
+            try {
+                socket = new Socket("localHost", 4445);
+                System.out.println("Connected");
+                isConnected = true;
+                outputStream = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+               
+               // Student student = new Student(1, "2006 ACE");
+                //System.out.println("Object to be written = " + student);
+                //outputStream.writeObject(student);
+              String string =new String("123456");
+               System.out.println("Object to be written = " + string);
+                outputStream.writeObject(string);
+                
+                
+              /*  ArrayList<String>  strings =new ArrayList<String>();
+                strings.add("123");
+                strings.add("321");
+               System.out.println("Object to be written = " + strings.get(0)+"/"+strings.get(1));
+                outputStream.writeObject(strings);  */
+                
+                
+                outputStream.flush();
+ 
+            } catch (SocketException se) {
+                se.printStackTrace();
+                System.exit(0);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        
-        clientSocket.close();
+    }
+ 
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.communicate();
     }
 }
--- Simulator/src/Server.java
+++ Simulator/src/Server.java
@@ -1,22 +1,69 @@
+
+
+
+/**
+ *
+ * @author qianru
+ */
 import java.io.*;
-import java.net.*;
-
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
 public class Server {
-	
-    public static void main(String args[]) throws Exception {
-        String clientSentence;
-        String capitalizedSentence;
-        ServerSocket welcomeSocket = new ServerSocket(5678);
-
-        while(true) {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
+    private ServerSocket serverSocket = null;
+    private Socket socket = null;
+    private ObjectInputStream inStream = null;
+ 
+ 
+    public Server() {
+ 
+    }
+ 
+    public void communicate() {
+        
+        try {
+            serverSocket = new ServerSocket(4445);
+            socket = serverSocket.accept();
+            System.out.println("Connected");
             
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
+            Student student1 = new Student(1, "Ace");
             
+     
+            inStream = new ObjectInputStream(new BufferedInputStream (socket.getInputStream()));
+            //Student student = (Student) inStream.readObject();
+            //System.out.println(student.getName());
+            //System.out.println("Object received = " + student);
+            
+            String st= (String)inStream.readObject();
+            System.out.println("Object received = " + st);
+            
+            switch(st){
+                case ("123456"): System.out.println("correct"); break;
+                case ("123"): System.out.println("wrong"); break;
+        
+        
+              }
+                   
+            
+           // ArrayList<String> st= (ArrayList<String>)inStream.readObject();
+           // System.out.println("Object received = " + st.get(0)+"/"+st.get(1));
+        
+            
+            socket.close();
+ 
+        } catch (SocketException se) {
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException cn) {
+            cn.printStackTrace();
         }
+    }
+ 
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.communicate();
     }
 }
